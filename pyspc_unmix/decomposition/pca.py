b421bd93f4320a6fa3edacca4468c7abf29312cc
Rustam Guliev <glvrst@gmail.com>
2025-02-03 10:45:26 +0100

    Fix __getitem__ for PCA decomposition

--- pyspc_unmix/decomposition/pca.py
+++ pyspc_unmix/decomposition/pca.py
@@ -1,3 +1,6 @@
+from copy import copy
+from typing import Tuple
+
 import numpy as np
 from sklearn.decomposition import PCA
 
@@ -14,4 +17,32 @@ class PCADecomposition(LinearDecomposition):
             scores=scores,
             names="PC",
             transformer=pca,
+        )
+
+    def __getitem__(self, given: Tuple[slice, slice, slice]) -> LinearDecomposition:
+        if not ((type(given) == tuple) and (len(given) == 3)):
+            raise ValueError(
+                "Invalid subset value. Provide 3 values in format <row, component, wl>"
+            )
+
+        i_spc, i_comp, i_wl = given
+        new_loadings = self._loadings[i_comp, i_wl]
+        new_scores = self._scores[i_spc, i_comp]
+        new_names = self.names[i_comp]
+
+        # Copy the transformer and update the components, explained variance, etc.
+        transformer: PCA = copy(self.transformer)
+        transformer.components_ = transformer.components_[i_comp, i_wl]
+        transformer.explained_variance_ = transformer.explained_variance_[i_comp]
+        transformer.explained_variance_ratio_ = transformer.explained_variance_ratio_[
+            i_comp
+        ]
+        transformer.singular_values_ = transformer.singular_values_[i_comp]
+        transformer.mean_ = transformer.mean_[i_wl]
+
+        return LinearDecomposition(
+            loadings=new_loadings,
+            scores=new_scores,
+            transformer=transformer,
+            names=new_names,
         )

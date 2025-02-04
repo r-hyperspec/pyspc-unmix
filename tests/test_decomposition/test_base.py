2ec413c7b2bf9167963b404e8c31a989fe8edf53
Rustam Guliev <glvrst@gmail.com>
2025-02-04 18:10:52 +0100

    Add tests for decompostion slicing

--- tests/test_decomposition/test_base.py
+++ tests/test_decomposition/test_base.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pytest
 
-from pyspc_unmix.decomposition.base import OLSTransformer
+from pyspc_unmix.decomposition.base import OLSTransformer, LinearDecomposition
+from pyspc_unmix.utils import generate_demo_mixture
 
 
 @pytest.fixture
@@ -35,3 +36,39 @@ def test_ols_basic_transform(sample_data):
     # Check shapes
     assert transformed.shape == C.shape
     assert reconstructed.shape == X.shape
+
+
+def test_decomposition_and_slicing():
+    X, C, S, wl = generate_demo_mixture()
+
+    # Limit to 10 samples for speed
+    X = X[:10, :]
+    C = C[:10, :]
+
+    # Test decomposition
+    dc = LinearDecomposition(loadings=S, scores=C)
+
+    assert dc.loadings.shape == S.shape
+    assert dc.scores.shape == C.shape
+    assert dc.names.tolist() == ["Comp1", "Comp2", "Comp3"]
+    assert isinstance(dc.transformer, OLSTransformer)
+
+    # Test slicing
+    dc_slice = dc[:, 1:, :]
+    np.testing.assert_equal(dc_slice.loadings, S[1:, :])
+    np.testing.assert_equal(dc_slice.scores, C[:, 1:])
+
+    dc_slice = dc[:, 1:, 20:30]
+    np.testing.assert_equal(dc_slice.loadings, S[1:, 20:30])
+    np.testing.assert_equal(dc_slice.scores, C[:, 1:])
+
+    dc_slice = dc[[0, 5], 1:, 20:30]
+    np.testing.assert_equal(dc_slice.loadings, S[1:, 20:30])
+    np.testing.assert_equal(dc_slice.scores, C[[0, 5], 1:])
+
+    # Single value slicing
+    dc_slice = dc[4, 1:, :]
+    np.testing.assert_equal(dc_slice.loadings, S[1:, :])
+    np.testing.assert_equal(dc_slice.scores, C[4, 1:])
+    assert dc_slice.D.shape == (len(wl),)
+    assert dc_slice.scores.shape == (2,)
